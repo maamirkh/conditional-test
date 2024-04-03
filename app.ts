79164604322328d2c105d40651243369f4075105
maamirkh <maamirkhan1983@gmail.com>
2024-04-03 23:43:31 +0500

    updated

---
+++ app.ts
@@ -0,0 +1,41 @@
+let car ='subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota');
+
+let bike = 'honda';
+console.log("Is bike == 'honda'? I predict True.");
+console.log(bike == 'honda');
+
+console.log("Is bike == 'suzuki'? I predict False");
+console.log(bike == 'suzuki')
+
+let drink = 'pepsi';
+console.log("Is drink == 'pepsi'? I prdict True");
+console.log(drink == 'pepsi');
+
+console.log("Is drink == 'coke'? I predict False.");
+console.log(drink == 'coke');
+
+let milk = 'nestle';
+console.log("Is milk == 'nestle'? I predict True.");
+console.log(milk == 'nestle');
+
+console.log("Is milk == 'olppers'? I predict False.");
+console.log(milk == 'olppers');
+
+let city = 'Karachi';
+console.log("Is city == 'Karachi'? I predict True.");
+console.log(city == 'Karachi');
+
+console.log("Is city == 'Lahore'? I predict False.");
+console.log(city == 'Lahore');
+
+let computer = 'Dell';
+console.log("Is computer == 'Dell'? I predict True");
+console.log(computer == 'Dell');
+
+console.log("Is computer == 'sony'? I predict False.");
+console.log(computer == 'sony');
